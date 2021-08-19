75b3b474b660d43459401031c8a9b22fb2d19543
binhnguyen159 <ngt.binh@tego.global>
2021-08-19 17:40:37 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/ninja_net_first/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ninja_net_first
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My first app 2"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text("Sasuke  ưqeqwe"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {},
+        ),
+      ),
+    ));
